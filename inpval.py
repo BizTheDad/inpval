0874e0c25b85340b32f1aa455bf5ae5840da5a61
BizTheDad <justin.paro@gmail.com>
2021-10-09 01:02:40 -0500

    Fixed option checking error

--- inpval.py
+++ inpval.py
@@ -11,27 +11,27 @@ def print_all_formats():
 
 if __name__ == "__main__":
     try:
-        if sys.argv[1].startswith('-'):
-            option = sys.argv[1]
-            if option == '-p' or option == '--pattern':
-                try:
-                    pattern = Formats.get_regex_pattern(sys.argv[2])
-                    sys.stdout.write(f"{pattern}\n")
-                except IndexError:
-                    sys.exit(f"inpval: Error: missing format for '{option}' option")
-                except UnknownFormatError:
-                    sys.exit(f"inpval: Error: unknown format: {sys.argv[2]}")
-            elif option == '-l' or option == '--list':
-                print_all_formats()
+        option = sys.argv[1]
+        match_against = sys.argv[2]
+        if option == '-p' or option == '--pattern':
+            try:
+                pattern = Formats.get_regex_pattern(match_against)
+                sys.stdout.write(f"{pattern}\n")
+            except IndexError:
+                sys.exit(f"inpval: Error: missing format for '{option}' option")
+            except UnknownFormatError:
+                sys.exit(f"inpval: Error: unknown format: {match_against}")
+        elif option == '-l' or option == '--list':
+            print_all_formats()
         else:
             try:
-                if Formats.matches_format(sys.argv[1], sys.argv[2]):
-                    sys.stdout.write(f"{sys.argv[2]}\n")
+                if Formats.matches_format(option, match_against):
+                    sys.stdout.write(f"{match_against}\n")
                 sys.exit(0)
             except IndexError:
                 sys.exit("inpval: Error: missing input string")
     except IndexError:
         sys.exit("Usage: inpval [OPTION] [FORMAT] [INPUT]")
     except UnknownFormatError:
-        sys.exit(f"inpval: Error: unknown format: {sys.argv[1]}")
+        sys.exit(f"inpval: Error: unknown format: {option}")
         
