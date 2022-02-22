d9817cc4ae3511f47bbbd7c084b76c85506343d6
BizTheDad <justin.paro@gmail.com>
2022-02-22 15:16:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='inpval',
     version='1.0',
     description='Input Validation Tool',
     author='Justin Paro',
-    author_email='justin.paro@gmail.com'
+    author_email='justin.paro@gmail.com',
+    packages=find_packages(include=['inpval*'])
 )
